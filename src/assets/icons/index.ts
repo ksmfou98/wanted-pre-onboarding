4a0823eec9959fbba5a9f57a23ed5507286515f0
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-01-10 16:17:16 +0900

    Feat: HeaderButtonGroup UI

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,5 @@
+export { ReactComponent as BellIcon } from "./bell.svg";
 export { ReactComponent as BetaIcon } from "./betaIcon.svg";
 export { ReactComponent as NewIcon } from "./newIcon.svg";
+export { ReactComponent as NIcon } from "./nIcon.svg";
+export { ReactComponent as SearchIcon } from "./search.svg";
--- src/components/HeaderButtonGroup.tsx
+++ src/components/HeaderButtonGroup.tsx
@@ -1,7 +1,51 @@
+import { NIcon, SearchIcon, BellIcon } from "assets/icons";
 import React, { ReactElement } from "react";
+import styled from "styled-components";
 
 function HeaderButtonGroup(): ReactElement {
-  return <div>HeaderButtonGroup</div>;
+  return (
+    <HeaderButtonGroupBlock>
+      <ButtonListStyled>
+        <ButtonItemStyled>
+          <SearchIcon />
+        </ButtonItemStyled>
+        <ButtonItemStyled>
+          <BellIcon />
+          <NewCircleStyled>
+            <NIcon />
+          </NewCircleStyled>
+        </ButtonItemStyled>
+      </ButtonListStyled>
+    </HeaderButtonGroupBlock>
+  );
 }
+
+const HeaderButtonGroupBlock = styled.aside`
+  padding: 9px 0;
+`;
+
+const ButtonListStyled = styled.div``;
+
+const ButtonItemStyled = styled.button`
+  position: relative;
+  display: inline-block;
+  height: 100%;
+  vertical-align: middle;
+  padding: 0 10px;
+  margin-top: 5px;
+`;
+
+const NewCircleStyled = styled.span`
+  position: absolute;
+  top: -6px;
+  left: 24px;
+  background-color: rgb(51, 102, 255);
+  width: 13px;
+  height: 13px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+`;
 
 export default HeaderButtonGroup;
