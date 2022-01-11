43e276e0d8fb0b1394cc8cad285f4dc67bbca3aa
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-01-11 14:27:52 +0900

    Feat: Banner 이미지 슬라이드 버튼 구현

--- src/components/BannerImageList.tsx
+++ src/components/BannerImageList.tsx
@@ -1,28 +1,115 @@
+import { ArrowLeftIcon, ArrowRightIcon } from "assets/icons";
 import { bannerImages } from "lib/bannerImages";
-import React, { ReactElement } from "react";
-import styled from "styled-components";
+import React, { ReactElement, useEffect, useRef, useState } from "react";
+import styled, { css } from "styled-components";
 import BannerImageListItem from "./BannerImageListItem";
 
+interface IButtonDirection {
+  direction: "left" | "right";
+}
+
 function BannerImageList(): ReactElement {
+  const [bannerImageList, setBannerImageList] = useState(
+    bannerImages.sort(() => Math.random() - 0.5)
+  );
+
+  useEffect(() => {
+    setBannerImageList([...bannerImages, ...bannerImages, ...bannerImages]);
+  }, []);
+
+  const slideRef = useRef<HTMLDivElement>(null);
+  const SHOW_SLIDE_LENGTH = 1;
+  const [currentSlide, setCurrentSlide] = useState(SHOW_SLIDE_LENGTH);
+
+  const onNextSlide = () => {
+    setCurrentSlide(currentSlide + SHOW_SLIDE_LENGTH);
+  };
+
+  const onBackSlide = () => {
+    setCurrentSlide(currentSlide - SHOW_SLIDE_LENGTH);
+  };
+
+  useEffect(() => {
+    if (slideRef.current) {
+      slideRef.current.style.transition = "all 0.5s ease-in-out";
+      slideRef.current.style.transform = `translateX(-${
+        1084 * (currentSlide - SHOW_SLIDE_LENGTH)
+      }px)`;
+    }
+  }, [currentSlide]);
+
   return (
-    <BannerImageListBlock>
-      <ImageList>
-        {bannerImages.map((image) => (
-          <BannerImageListItem key={image.id} imageItem={image} />
-        ))}
-      </ImageList>
-    </BannerImageListBlock>
+    <>
+      <BannerImageListBlock ref={slideRef}>
+        <ImageList>
+          {bannerImageList.map((image) => (
+            <BannerImageListItem key={image.id} imageItem={image} />
+          ))}
+        </ImageList>
+      </BannerImageListBlock>
+
+      <ArrowButtonStyled direction="left" onClick={onBackSlide}>
+        <IconBox>
+          <ArrowLeftIcon />
+        </IconBox>
+      </ArrowButtonStyled>
+
+      <ArrowButtonStyled direction="right" onClick={onNextSlide}>
+        <IconBox>
+          <ArrowRightIcon />
+        </IconBox>
+      </ArrowButtonStyled>
+    </>
   );
 }
 
 const BannerImageListBlock = styled.div`
-  position: relative;
-  overflow: hidden;
-  padding: 0 50px;
+  width: 100%;
+  position: absolute;
+  width: 100%;
+  left: -4336px;
 `;
 
 const ImageList = styled.div`
-  width: 57024px;
+  display: flex;
+`;
+
+const ArrowButtonStyled = styled.button<IButtonDirection>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 120px;
+  width: 30px;
+  height: 60px;
+  opacity: 0.5;
+  border-radius: 15px;
+  background-color: #fff;
+  font-size: 16px;
+  ${(props) =>
+    props.direction === "left"
+      ? css`
+          left: calc((100% - 1210px) / 2);
+        `
+      : css`
+          right: calc((100% - 1200px) / 2);
+        `}
+`;
+
+const IconBox = styled.span`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: inherit;
+  svg {
+    user-select: none;
+    width: 1em;
+    height: 1em;
+    display: inline-block;
+    fill: currentColor;
+    flex-shrink: 0;
+    font-size: inherit;
+  }
 `;
 
 export default BannerImageList;
--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -1,74 +1,20 @@
-import { ArrowLeftIcon, ArrowRightIcon } from "assets/icons";
 import React, { ReactElement } from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import BannerImageList from "./BannerImageList";
-
-interface IButtonDirection {
-  direction: "left" | "right";
-}
 
 function TopBanner(): ReactElement {
   return (
     <TopBannerBlock>
       <BannerImageList />
-
-      <ArrowButtonStyled direction="left">
-        <IconBox>
-          <ArrowLeftIcon />
-        </IconBox>
-      </ArrowButtonStyled>
-
-      <ArrowButtonStyled direction="right">
-        <IconBox>
-          <ArrowRightIcon />
-        </IconBox>
-      </ArrowButtonStyled>
     </TopBannerBlock>
   );
 }
 
 const TopBannerBlock = styled.div`
   padding-top: 25px;
+  width: 1084px;
+  margin: 0 auto;
   position: relative;
-  /* overflow: hidden; */
-`;
-
-const ArrowButtonStyled = styled.button<IButtonDirection>`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  top: 120px;
-  width: 30px;
-  height: 60px;
-  opacity: 0.5;
-  border-radius: 15px;
-  background-color: #fff;
-  font-size: 16px;
-  ${(props) =>
-    props.direction === "left"
-      ? css`
-          left: calc((100% - 1210px) / 2);
-        `
-      : css`
-          right: calc((100% - 1200px) / 2);
-        `}
-`;
-
-const IconBox = styled.span`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: inherit;
-  svg {
-    user-select: none;
-    width: 1em;
-    height: 1em;
-    display: inline-block;
-    fill: currentColor;
-    flex-shrink: 0;
-    font-size: inherit;
-  }
 `;
 
 export default TopBanner;
